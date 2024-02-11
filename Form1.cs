b89c6dd58fc3b5a6af6c122cc1fd6e78dd572e18
Dimlenn <d_melen@mail.ru>
2024-02-11 15:08:56 +0300

    git

---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace Lab10;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Lab10/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+            try
+            {
+                double c = double.Parse(a) + double.Parse(b);
+                textBox3.Text = c.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка");
+                Application.Restart();
+            }
+        }
+    }
+}
